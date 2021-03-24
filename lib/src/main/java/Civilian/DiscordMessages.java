3cb9602d122fb382899a143183fba8da89b5f1df
Soren Pidgeon <pidgeon.soren@gmail.com>
2021-03-24 19:23:57 +0800

    feature update to include: highlight guilds in log, multistream

--- lib/src/main/java/Civilian/DiscordMessages.java
+++ lib/src/main/java/Civilian/DiscordMessages.java
@@ -6,14 +6,20 @@ import net.dv8tion.jda.api.EmbedBuilder;
 
 
 public class DiscordMessages extends ListenerAdapter {
-	@Override
+	
+    public static final String ANSI_SOMNI = "\033[38;2;34;31;255m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    @Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		JDA jda = event.getJDA();
 		User author = event.getAuthor();
 		Message message = event.getMessage();
-
+        Guild guild = event.getGuild();
+        MessageChannel channel = event.getChannel();
 		String msg = message.getContentDisplay();
-
+        String guildColour = "";
+        String channelColour = "";
 		if (author.isBot() == true) {
 			return;
 		} else {
@@ -21,17 +27,24 @@ public class DiscordMessages extends ListenerAdapter {
 				// DEFAULT FUNCTIONS
 				if (event.getChannel().getId().equals("820279211704057927")) {
 				} else {
-					System.out.println("[" + event.getGuild().getName() + ": " + event.getChannel().getName() + "] " + author.getName() + ": " + msg);
+                    if (guild.getName().equals("ins0mnia's World")) {
+                        guildColour = ANSI_SOMNI;
+                    }
+					
+                    System.out.println(guildColour + "[" + event.getGuild().getName() + ": " + event.getChannel().getName() + "] " + ANSI_RESET  + author.getName() + ": " + msg);
 				}
 				// COMMANDS
 			}
 		}
 	}
 
-	public static void PostTwitchLive(EmbedBuilder embed) {
-		Civilian.jda.getTextChannelById("731702663884046408").sendMessage("Ins0mnia is going live now <@&748788513872412702>").queue();
-		Civilian.jda.getTextChannelById("731702663884046408").sendMessage(embed.build()).queue();
-		embed.clear();
+	public static void PostTwitchLive(EmbedBuilder goingLiveEmbed, String streamName) {
+        String goingLiveNow = streamName + " is going live now";
+        if (streamName.equals("ins0mniasr")) {
+            goingLiveNow = goingLiveNow + " <@&748788513872412702>";
+        }
+		Civilian.jda.getTextChannelById("731702663884046408").sendMessage(goingLiveNow).embed(goingLiveEmbed.build()).queue();
+		goingLiveEmbed.clear();
 	}
 
 	public static void PostMessage(String message) {
--- lib/src/main/java/Civilian/TwitchManager.java
+++ lib/src/main/java/Civilian/TwitchManager.java
@@ -1,15 +1,16 @@
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.events.ChannelGoLiveEvent;
+import com.github.twitch4j.helix.domain.User;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.awt.*;
+import java.util.Collections;
 import java.util.Locale;
 
 public class TwitchManager {
     public static final String twitchId = "32p3en14qg28y227gim6phritbu1la";
     public static final String twitchSecret = "q6z3biu1wbo26lgrczly90thm1ht2g";
-    public static final String targetStreamer = "ins0mniasr";
     public static final Color SomniBlue = new Color(0, 102, 255);
 
     public static void StartTwitchClient() {
@@ -20,29 +21,32 @@ public class TwitchManager {
                 .withClientSecret(twitchSecret)
                 .build();
 
-        twitchBot.getClientHelper().enableStreamEventListener(targetStreamer);
-        System.out.println("StreamEventListener active on: " + targetStreamer);
+        twitchBot.getClientHelper().enableStreamEventListener("ins0mniasr");
+        twitchBot.getClientHelper().enableStreamEventListener("sozapidgeon");
+        System.out.println("StreamEventListener active");
         twitchBot.getEventManager().onEvent(ChannelGoLiveEvent.class, event -> {
-//		DiscordMessages.PostMessage("Test for bot");
-        	EmbedBuilder goingLive = new EmbedBuilder();
-        	goingLive.setColor(SomniBlue);
+            
+            String streamName = event.getChannel().getName();
+            String streamUrl = ("https://www.twitch.tv/" + streamName);
+            User streamUser = twitchBot.getHelix().getUsers(null, null, Collections.singletonList(streamName)).execute().getUsers().get(0);
+            String streamUserPictureUrl = streamUser.getProfileImageUrl();
+            String streamTitle = event.getStream().getTitle();
+            String streamGame = event.getStream().getGameName();
 
-//            embed=discord.Embed(title=Hollow Knight Al2ba%  and then Nice% Speedruns, url=https://www.twitch.tv/ins0mniasr, color=0x221fff)
-//            embed.set_author(name=ins0mniaSR, url=https://www.twitch.tv/ins0mniasr, icon_url=https://images-ext-2.discordapp.net/external/WsMRlkdt8n8CMFR2E8tH-YAt9SNMo5DnDjrIHH7bKpI/https/static-cdn.jtvnw.net/jtv_user_pictures/1e765843-8642-41ef-b62e-19fec401eb46-profile_image-300x300.png)
-//            embed.set_thumbnail(url=https://images-ext-2.discordapp.net/external/WsMRlkdt8n8CMFR2E8tH-YAt9SNMo5DnDjrIHH7bKpI/https/static-cdn.jtvnw.net/jtv_user_pictures/1e765843-8642-41ef-b62e-19fec401eb46-profile_image-300x300.png?width=120&height=120)
-//            embed.add_field(name=Game, value=Hollow Knight, inline=True)
-//            await ctx.send(embed=embed)
+            System.out.println("Stream started for " + streamName + ": " + streamTitle);
 
+            EmbedBuilder goingLive = new EmbedBuilder();
+        	
+            goingLive.setColor(SomniBlue);
+            goingLive.setTitle(streamTitle, streamUrl);
+            goingLive.setAuthor(streamName + " just went live!", null, streamUserPictureUrl);
+            goingLive.setThumbnail(streamUserPictureUrl);
+            goingLive.addField("Game", streamGame, false);
 
-
-
-            goingLive.setTitle(event.getStream().getTitle(), "https://www.twitch.tv/ins0mniasr");
-            goingLive.setAuthor("ins0mniaSR just went live!", "https://images-ext-2.discordapp.net/external/WsMRlkdt8n8CMFR2E8tH-YAt9SNMo5DnDjrIHH7bKpI/https/static-cdn.jtvnw.net/jtv_user_pictures/1e765843-8642-41ef-b62e-19fec401eb46-profile_image-300x300.png");
-            goingLive.setThumbnail("https://images-ext-2.discordapp.net/external/WsMRlkdt8n8CMFR2E8tH-YAt9SNMo5DnDjrIHH7bKpI/https/static-cdn.jtvnw.net/jtv_user_pictures/1e765843-8642-41ef-b62e-19fec401eb46-profile_image-300x300.png");
-            goingLive.addField("Game", event.getStream().getGameName(), false);
-
-            DiscordMessages.PostTwitchLive(goingLive);
+            DiscordMessages.PostTwitchLive(goingLive, streamName);
+            
             goingLive.clear();
+
         });
     }
 }
